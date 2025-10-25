61fa5a691958166d85a5168b721617633bb46e1a
KraLeoD <165140689+KraLeoD@users.noreply.github.com>
2025-10-25 19:39:41 +0200

    Add netcup provider for DNS management
    Added netcup provider configuration for DNS management.

--- .demo/providers.tf
+++ .demo/providers.tf
@@ -18,6 +18,10 @@ terraform {
       source  = "hashicorp/time"
       version = ">= 0.11.0"
     }
+    netcup = {
+      source  = "aellwein/netcup"
+      version = ">= 0.1.3"
+    }
   }
 }
 
@@ -36,4 +40,11 @@ provider "kubernetes" {
   client_certificate     = module.talos.kubeconfig_data.client_certificate
   client_key             = module.talos.kubeconfig_data.client_key
   cluster_ca_certificate = module.talos.kubeconfig_data.cluster_ca_certificate
+}
+
+# netcup provider for DNS management
+provider "netcup" {
+  customer_id  = var.netcup_customer_id
+  api_key      = var.netcup_api_key
+  api_password = var.netcup_api_password
 }
