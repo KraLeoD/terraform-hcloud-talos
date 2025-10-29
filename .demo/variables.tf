0fdbdc0f8126b71376820ffbfe2d25bdeb0c309c
KraLeoD <165140689+KraLeoD@users.noreply.github.com>
2025-10-29 19:40:57 +0100

    Remove Netcup DNS configuration variables
    Removed Netcup DNS configuration variables from variables.tf.

--- .demo/variables.tf
+++ .demo/variables.tf
@@ -3,28 +3,3 @@ variable "hcloud_token" {
   description = "Hetzner Cloud API token"
   sensitive   = true
 }
-
-# Netcup DNS Configuration Variables
-variable "netcup_domain" {
-  type        = string
-  description = "Your Netcup domain (e.g., example.com)"
-  sensitive   = false
-}
-
-variable "netcup_customer_id" {
-  type        = string
-  description = "Netcup Customer ID (found in CCP under API)"
-  sensitive   = true
-}
-
-variable "netcup_api_key" {
-  type        = string
-  description = "Netcup API Key"
-  sensitive   = true
-}
-
-variable "netcup_api_password" {
-  type        = string
-  description = "Netcup API Password"
-  sensitive   = true
-}
